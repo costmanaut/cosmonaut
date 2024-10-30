cfec5e68fc2edfb9dd97064817f6d8bb134d319f
costmanaut <yoqutg@gmail.com>
2024-10-30 12:06:21 +0500

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,21 +23,21 @@ function createStars(count) {
 
 function drawStars() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   stars.forEach(star => {
     ctx.beginPath();
     ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
     const alpha = (Math.sin(star.phase) + 1) / 2; // Используем синус для плавного мерцания
     ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
     ctx.fill();
-    
+
     // Мерцание звезды
     star.phase += star.alphaChangeSpeed;  // Плавное изменение прозрачности
-    
+
     // Движение звёзд (медленное)
     star.x += star.dx;
     star.y += star.dy;
-    
+
     // Переход звёзд по краям экрана (если выходит за пределы, то появляется с другой стороны)
     if (star.x < 0) star.x = canvas.width;
     if (star.x > canvas.width) star.x = 0;
@@ -61,3 +61,61 @@ window.addEventListener('resize', () => {
   stars = [];
   createStars(300);
 });
+
+
+
+
+// текст
+
+const phrases = [
+  "Сайт для Вашего Бизнеса",
+  "Сайт это Ваша Бизнес"
+];
+let currentPhraseIndex = 0;
+let letterIndex = 0;
+let delay = 100;
+let typingDelay = 100;
+let erasingDelay = 50;
+let newPhraseDelay = 500;
+
+const textElement = document.getElementById("text");
+
+function type() {
+  const currentPhrase = phrases[currentPhraseIndex];
+
+  // Если все буквы напечатаны, задержка перед стиранием
+  if (letterIndex < currentPhrase.length) {
+    textElement.textContent += currentPhrase[letterIndex];
+    letterIndex++;
+
+    // Задержка для пробела
+    if (currentPhrase[letterIndex - 1] === " ") {
+      setTimeout(type, newPhraseDelay);
+    } else {
+      setTimeout(type, typingDelay);
+    }
+  } else {
+    // Задержка перед стиранием текста
+    setTimeout(erase, newPhraseDelay);
+  }
+}
+
+function erase() {
+  const currentPhrase = phrases[currentPhraseIndex];
+
+  // Удаление букв по одной
+  if (letterIndex > 0) {
+    textElement.textContent = currentPhrase.substring(0, letterIndex - 1);
+    letterIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    // Смена фразы и запуск печати новой
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    setTimeout(type, newPhraseDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, newPhraseDelay);
+});
+
